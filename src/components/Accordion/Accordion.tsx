85c57de2d450191de1fd02e745a5ffcbdb87d943
MaksimSinicbIn <dzigen1986@gmail.com>
2024-02-21 14:43:25 +0700

    Refactoring component to controlled

--- src/components/Accordion/Accordion.tsx
+++ src/components/Accordion/Accordion.tsx
@@ -3,28 +3,14 @@ import React from "react"
 type AccordionPropsType = {
     titleValue: string
     collapsed: boolean
+    onChange: () => void
 }
-
-// let a = true && true && true;
-// a = true;
-// let a = true && false && true;
-// a = false;
-// let a = true && true && false;
-// a = false;
-// let a = "yo" && true && true;
-// a = true;
-// let a = true && true && "yo";
-// a = "yo"; (true) - псевдоистина
-// let a = "" && true && true;
-// a = ""; (false) - псевдоложь
-// let a = true && true && "";
-// a = ""; (false) - псевдоложь
 
 function Accordion(props: AccordionPropsType) {
     console.log("Accordion rendering")
         return (
             <>
-                <AccordionTitle title={props.titleValue} />
+                <AccordionTitle title={props.titleValue} onChange={props.onChange} />
                 {/* { props.collapsed === false && <AccordionBody /> } */}
                 {/* ! - отрицание (если не) = дает противоположное значение */}
                 { !props.collapsed && <AccordionBody /> }
@@ -34,12 +20,12 @@ function Accordion(props: AccordionPropsType) {
 
 type AccordionTitlePropsType = {
     title: string
+    onChange: () => void
 }
 
 function AccordionTitle(props: AccordionTitlePropsType) {
-    console.log("AccordionTitle rendering")
     return (
-        <h3>{ props.title }</h3>
+        <h3 onClick={props.onChange}>{ props.title }</h3>
     )
 }
 
